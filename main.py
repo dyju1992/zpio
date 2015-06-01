5d3c8a411edcb2fa4e6ec852b5ac9fb34b48106d
Przemysław Dyjak <dyju1992@gmail.com>
2015-06-01 22:11:02 +0200

    Fixed algoritm to evoluated Fibonacci numbers

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ __author__ = 'dyju'
 Wynik=[1,2]
 
 for i in range (2,100):
-    Wynik.append(Wynik[i-1]+Wynik[i-1])
+    Wynik.append(Wynik[i-1]+Wynik[i-2])
     if Wynik[i]<4000000:
         print(Wynik[i])
